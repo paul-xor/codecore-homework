b59f71d25b50f7e7cc07c575dfdf6db1d74b5725
Pavel Smolov <palexsmol@gmail.com>
2018-11-23 00:41:30 -0800

    forward method implemented

--- week-2/turtle.js
+++ week-2/turtle.js
@@ -1,7 +1,7 @@
 
-// 	 🐢
+// 	 
 //    0 1 2 3 4         
-// 	0 o + + o +
+// 	0 🐢 + + o +
 // 	1 + + + + +
 // 	2 + + + + +
 // 	3 + + + x +
@@ -21,7 +21,35 @@ class Turtle {
     get getDirection(){
         return `${this.dir}`;
     }
+    forward(n){
+        this.n = n;
+        
+		switch(this.dir) {
+            case 90:
+            return this.y = rangeCheckMinus(this.y,n);
+            break;
+            case 180:
+            return this.x = rangeCheckMinus(this.x,n);
+            break;
+            case 270:
+            return this.y = rangeCheckPlus(this.y,n);
+            break;
+
+            default: // 0
+            return this.x = rangeCheckPlus(this.x,n);
+        }
+	}
+
 };
+
+let fieldRange = 5;
+function rangeCheckPlus (start,next){
+    return (0 <= start + next && start + next < fieldRange) ? start + next : 'Out Of Range!'
+}
+
+function rangeCheckMinus (start,next){
+    return (0 <= start - next && start - next < fieldRange) ? start - next : 'Out Of Range!'
+}
 
 // Recommended approach for variable setter x
 Object.defineProperty(Turtle, 'x', {
@@ -39,12 +67,19 @@ Object.defineProperty(Turtle, 'dir', {
     writable: true
 });
 
+// ----------< TEST >------------ //
+
 //init Turtle in (0,0)
 let flash = new Turtle(0,0);
+flash.forward(3);
+console.log(flash.getPosition);
+flash.dir = 270;
+flash.forward(3);
 console.log(flash.getPosition);
 
-flash.x = 1;
-flash.y = 2;
-console.log(flash.getPosition);
-console.log(flash.getDirection);
+// flash.x = 1;
+// flash.y = 2;
+// console.log(flash.getPosition);
+// console.log(flash.getDirection);
+
 
